5e0997b90d414e2632cb0b1ab69f4331d9eaf02d
zkyredstart <zhoukunyang@xiaohongshu.com>
2024-09-02 10:16:11 +0800

    Create tokenpacker.py

---
+++ llava/model/multimodal_projector/tokenpacker.py
@@ -0,0 +1,102 @@
+class TokenPacker(nn.Module):
+    def __init__(
+            self,
+            raw_grid=24,
+            embed_dim=1024,
+            num_heads=1024//128,
+            kv_dim=1024,
+            hidden_size=4096,
+            scale_factor=2,
+            norm_layer=partial(nn.LayerNorm, eps=1e-6)
+    ):
+        super().__init__()
+        if raw_grid%scale_factor!=0:
+            raise ValueError("scale_factor must be divisible by grid size")
+        self.raw_grid = raw_grid
+        self.grid_size = raw_grid//scale_factor
+        self.num_queries = self.grid_size ** 2
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.scale_factor = scale_factor
+        self.q_proj_1 = nn.Linear(kv_dim, embed_dim, bias=False)
+
+        k_modules = [nn.Linear(4096, 1024)]
+        for _ in range(1,2):
+            k_modules.append(nn.GELU())
+            k_modules.append(nn.Linear(1024, 1024))
+        self.k_proj_1 = nn.Sequential(*k_modules)
+
+        v_modules = [nn.Linear(4096, 1024)]
+        for _ in range(1,2):
+            v_modules.append(nn.GELU())
+            v_modules.append(nn.Linear(1024, 1024))
+        self.v_proj_1 = nn.Sequential(*v_modules)
+
+        self.ln_q_1 = norm_layer(embed_dim)
+        self.ln_k_1 = norm_layer(embed_dim)
+        self.ln_v_1 = norm_layer(embed_dim)
+
+        self.clip_attn = MultiheadAttention(embed_dim, num_heads)
+
+        modules = [nn.Linear(1024, hidden_size)]
+        for _ in range(1, 2):
+            modules.append(nn.GELU())
+            modules.append(nn.Linear(hidden_size, hidden_size))
+        self.mlp = nn.Sequential(*modules)
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    def divide_feature(self, x, kernel_size, token_num, N, c):
+        h = w = int(token_num**0.5)
+
+        reshape_x = x.reshape(h, w, N, c).reshape(h//kernel_size, kernel_size, w, N, c)
+        reshape_x = reshape_x.permute(0,2,1,3,4)
+        reshape_x = reshape_x.reshape(h//kernel_size, w//kernel_size, kernel_size, kernel_size, N, c)
+        reshape_x = reshape_x.permute(0,1,3,2,4,5).reshape(h//kernel_size, w//kernel_size, kernel_size*kernel_size, N, c)
+        reshape_x = reshape_x.permute(2,0,1,3,4).reshape(kernel_size*kernel_size, -1, c)
+
+        return reshape_x
+
+    def forward(self, x, attn_mask=None):
+
+        x_multi = x[1] # mulit-level
+        x = x[0] # original single-level
+
+        key = self.ln_k_1(self.k_proj_1(x_multi)).permute(1, 0, 2)
+        value = self.ln_v_1(self.v_proj_1(x_multi)).permute(1, 0, 2)
+
+        token_num, N, c = key.shape
+
+        q = F.interpolate(x.reshape(x.shape[0],self.raw_grid,self.raw_grid,-1).float().permute(0,3,1,2), size=(self.grid_size, self.grid_size), mode='bilinear').permute(0,2,3,1) ## fix
+        q = q.reshape(q.shape[0], -1, q.shape[-1]).to(x.dtype)
+
+        query = self.ln_q_1(self.q_proj_1(q)).permute(1, 0, 2)
+
+        reshape_query = self.divide_feature(query, 1, self.num_queries, N, c)
+        reshape_key = self.divide_feature(key, self.scale_factor, token_num, N, c)
+        reshape_value = self.divide_feature(value, self.scale_factor, token_num, N, value.shape[-1])
+
+        out = self.clip_attn(
+            reshape_query,
+            reshape_key,
+            reshape_value,
+            attn_mask=attn_mask)[0]
+
+        x = out
+        x = x.reshape(self.num_queries, N, -1)
+        x = x.permute(1, 0, 2)
+
+        x = self.mlp(x)
+        return x
+
+    def _repeat(self, query, N: int):
+        return query.unsqueeze(1).repeat(1, N, 1)
